751e7071ac458fa2577b7f543821e5fcf8d1d958
Rachel Rost <rachel.v.rost@gmail.com>
2016-09-19 11:28:41 -0500

    Updated Exercise with Correct Solution

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,5 +1,16 @@
- USE codeup_test_db;
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
 
+CREATE TABLE albums (
 
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	artist VARCHAR(80) NOT NULL,
+	name VARCHAR(150) NOT NULL,
+	release_date DATE NOT NULL,
+	sales FLOAT(10,2) UNSIGNED NOT NULL, 
+	genre VARCHAR(20) NOT NULL,
+	PRIMARY KEY (id),
+	UNIQUE (artist, name)
 
- 
+);
+	
